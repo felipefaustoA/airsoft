39a3622be7968b7a9a7e5d75cb846d18da4dc9d3
Felipe Fausto <fellipe.fausto10@gmail.com>
2025-08-25 17:06:42 -0300

    initial commit

---
+++ Player/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody3D
+
+@export var speed = 10.0
+@export var mouse_sensitivity = 0.2
+var gravity = -9.8
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		$Camera3D.rotation.x -= deg_to_rad(event.relative.y * mouse_sensitivity)
+		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+		self.rotation.y -= deg_to_rad(event.relative.x * mouse_sensitivity)
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
