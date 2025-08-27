4e3d0e54f6e6dcfe36787720b838140c52d4ac7b
Felipe Fausto <fellipe.fausto10@gmail.com>
2025-08-27 20:38:25 -0300

    add: magnus effect

--- Player/Weapon/bb.gd
+++ Player/Weapon/bb.gd
@@ -1,10 +1,10 @@
 extends RigidBody3D
 
-@export var energy: float = 1.49 # Joules
+@export var energy: float = 1.49 # joules
 @export var bb_mass: float = 0.0002 # kg
 @export var drag_coefficient : float = 0.47
-@export var dir: Vector3
-@export var ball_radius : float = 0.003
+@export var ball_radius : float = 0.003 # mm
+@export var magnus_coefficient : float = 0.0000012 
 
 const AIR_DENSITY : float = 1.225 # kg/m³
 
@@ -15,15 +15,16 @@ func _ready() -> void:
 	ball_area = PI * pow(ball_radius, 2)
 	
 func _physics_process(delta: float) -> void:
+	if not has_drag:
+		return
+		
 	var speed = linear_velocity.length()
 	
-	if speed > 0 and has_drag:
+	if speed > 0:
 		var drag_force = 0.5 * AIR_DENSITY * pow(speed, 2) * drag_coefficient * ball_area
 		var drag_dir = -linear_velocity.normalized()
-		var final_drag_force = drag_force * drag_dir
+		apply_central_force(drag_force * drag_dir)
 		
-		apply_central_force(final_drag_force)
-		
-		
-#func _process(delta: float) -> void:
-	#print(global_position.z)
+		var magnus_direction = angular_velocity.cross(linear_velocity).normalized()
+		var magnus_force_magnitude = magnus_coefficient * pow(speed, 2)
+		apply_central_force(magnus_force_magnitude * magnus_direction)
--- Player/Weapon/weapon.gd
+++ Player/Weapon/weapon.gd
@@ -12,22 +12,23 @@ func _input(event: InputEvent):
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("change_drag"):
 		has_drag = !has_drag
-		print(has_drag)
+		print("Arrasto ativado: ", has_drag)
 
 func shoot():
 	var bb = bb_scene.instantiate()
 	get_tree().root.add_child(bb)
 	
-	bb.has_drag = has_drag
-	
 	bb.global_transform.origin = $Muzzle.global_transform.origin
 	
-	var v_mps = sqrt(2 * bb.energy / bb.bb_mass)
+	bb.has_drag = has_drag
 	
+	var v_mps = sqrt(2 * bb.energy / bb.bb_mass)
 	var v_fps = v_mps * METERS_TO_FEET
 	
 	print("Velocidade do projétil: ", v_fps, " pés/segundo (ft/s)")
 	
-	bb.dir = -$Muzzle.global_transform.basis.z.normalized()
+	var fire_dir = -$Muzzle.global_transform.basis.z.normalized()
+	var backspin_axis = fire_dir.cross(Vector3.UP).normalized()
 	
-	bb.linear_velocity = bb.dir * v_mps
+	bb.linear_velocity = fire_dir * v_mps
+	bb.angular_velocity = 150 * backspin_axis
